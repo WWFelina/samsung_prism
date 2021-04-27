282a1b05f41bf763f6d5b0e41239fbe76d691af3
WWFelina <prabh.070999@hotmail.com>
2021-04-27 20:10:44 +0530

    Breaking down data_gen.py into functions for better readability

--- data_generation/data_gen.py
+++ data_generation/data_gen.py
@@ -1,41 +1,49 @@
-
 import re
 
-#opening file
-with open("alexa.txt", "r") as f:
-    content = f.read()
+def get_commands(text):
+    #need all text between " "
+    pattern = r'"([^"]*)"'
+    m = re.findall(pattern, content)
 
-#need all text between " "
-pattern = r'"([^"]*)"'
+    #Removing the alexa keyword
+    #Removing punctuation
+    for i in range(len(m)):
+        m[i] = m[i].replace("Alexa, ", "")
+        m[i] = re.sub(r"[.!?\-,]", "", m[i])
+        m[i] = m[i].lower()
+    return m
 
-m = re.findall(pattern, content)
+def unique_words(commands):
+    word_dict = {}
+    for command in commands:
+        for word in command.split():
+            if word not in word_dict:
+                word_dict[word] = 1
+    
+    return word_dict
 
-#Removing the alexa keyword
-#Removing periods/commas at the end if there
-for i in range(len(m)):
-    m[i] = m[i].replace("Alexa, ", "")
-    if m[i][-1] == "," or m[i][-1] == ".":
-        m[i] = m[i][:-1]
-    m[i] = m[i].lower()
-print(f"Total number of commands : {len(m)}")
-print(f"All commands : {m}")
+def find_placeholders(commands):
+    to_generate = []
+    pattern2 = r'\[[A-Za-z0-9 \-]*\]'
+    for item in commands:
+        placeholders = re.findall(pattern2, item)
+        if placeholders:
+            to_generate.extend(placeholders)
+    return to_generate
 
-to_generate = []
-pattern2 = r'\[[A-Za-z0-9 \-]*\]'
-for item in m:
-    placeholders = re.findall(pattern2, item)
-    if placeholders:
-        to_generate.extend(placeholders)
+if __name__ == "__main__":
+    #opening file
+    with open("alexa.txt", "r") as f:
+        content = f.read()
 
-print(f"Total number of customisable commands : {len(to_generate)}")
-print(f"All customisable keywords : {to_generate}")
+    m = get_commands(content)
+    print(f"Total number of commands : {len(m)}")
+    print(f"All commands : {m}")
 
-'''finding number of unique words
-word_dict = {}
-for command in m:
-    for word in command.split():
-        if word not in word_dict:
-            word_dict[word] = 1
+    word_dict = unique_words(m)
+    print(word_dict)
+    print(len(word_dict))
 
-print(word_dict)
-print(len(word_dict))'''
+    customisable = find_placeholders(m)
+    print(f"Total number of customisable commands : {len(customisable)}")
+    print(f"All customisable keywords : {set(customisable)}")
